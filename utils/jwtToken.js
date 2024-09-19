c11fef3fae00ef32148e73846a4d3481010c4ba3
Mikiyas Dawit <90477617+MAYSHLMAY@users.noreply.github.com>
2024-09-20 02:06:26 +0300

    Update jwtToken.js

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -1,17 +1,50 @@
-export const generateToken = (user, message, statusCode, res) => {
-  const token = user.generateJsonWebToken();
-  res
-    .status(statusCode)
-    .cookie("token", token, {
-      expires: new Date(Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
-      httpOnly: true,
-      secure: process.env.NODE_ENV === 'production', // Only set to true in production
-      sameSite: 'None', // Required for cross-origin cookies
-    })
-    .json({
-      success: true,
-      message,
-      user,
-      token,
-    });
-};
+import express from "express";
+import dotenv from 'dotenv';
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import fileUpload from "express-fileupload";
+import { connection } from "./database/connection.js";
+import { errorMiddleware } from "./middlewares/error.js";
+import userRouter from "./router/userRoutes.js";
+import auctionItemRouter from "./router/auctionItemRoutes.js";
+import bidRouter from "./router/bidRoutes.js";
+import commissionRouter from "./router/commissionRouter.js";
+import superAdminRouter from "./router/superAdminRoutes.js";
+import { endedAuctionCron } from "./automation/endedAuctionCron.js";
+import { verifyCommissionCron } from "./automation/verifyCommissionCron.js";
+
+const app = express();
+dotenv.config();
+
+
+app.use(
+  cors({
+    origin: ["https://bid-sphere.vercel.app"], // Explicitly allow your frontend's origin
+    methods: ["POST", "GET", "PUT", "DELETE"],
+    credentials: true, // This allows cookies and other credentials to be sent
+  })
+);
+
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "/tmp/",
+  })
+);
+
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/auctionitem", auctionItemRouter);
+app.use("/api/v1/bid", bidRouter);
+app.use("/api/v1/commission", commissionRouter);
+app.use("/api/v1/superadmin", superAdminRouter);
+
+endedAuctionCron();
+verifyCommissionCron();
+connection();
+app.use(errorMiddleware);
+
+export default app;
