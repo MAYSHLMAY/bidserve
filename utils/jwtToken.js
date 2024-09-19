0c2643e308381e860fa553fe689bc81dd6689568
Mike <educationforlife424@gmail.com>
2024-09-20 02:38:03 +0300

    Done

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -3,10 +3,10 @@ export const generateToken = (user, message, statusCode, res) => {
   res
     .status(statusCode)
     .cookie("token", token, {
-      expires: new Date(
-        Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
-      ),
+      expires: new Date(Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
       httpOnly: true,
+      secure: false, 
+      sameSite: 'None',
     })
     .json({
       success: true,
